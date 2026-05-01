42cc53708fca25e4af3c10f8f511eef8e53b5553
Luiz Alberto Huller da Silva <95250997+LuizAlbertoDev@users.noreply.github.com>
2026-05-01 00:01:14 -0300

    att

---
+++ Desafios_JS/Desafio12.js
@@ -0,0 +1,6 @@
+const usuariosBrutos = [
+    { id: 1, nome: "luiz gustavo", email: "LUIZ@GMAIL.COM", idade: 25 },
+    { id: 2, nome: "ana beatriz", email: "ana@Yahoo.com", idade: 0 },
+    { id: 3, nome: "CARLOS SILVA", email: "carlos@outlook.com", idade: 17 },
+    { id: 4, nome: "jose almeida", email: "JOSE@gmail.com", idade: 40 }
+];
