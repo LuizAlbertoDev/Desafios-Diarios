b7e09ecaaf99f2224bdce8e5bac978b999859d24
Luiz Alberto Huller da Silva <95250997+LuizAlbertoDev@users.noreply.github.com>
2026-05-03 20:03:45 -0300

    Aprendendo JSON
    Aprendendo JSON

---
+++ Desafios_JS/Desafio_041/Desafio_041.js
@@ -0,0 +1,51 @@
+const tarefas = JSON.parse(localStorage.getItem("minhas_tarefas")) || [];
+
+
+function salvar(){
+    const listaEmTexto = JSON.stringify(tarefas)
+    localStorage.setItem("minhas_tarefas",listaEmTexto)
+}
+
+const nomeTarefa = document.getElementById("tarefa")
+const lista = document.getElementById("lista")
+
+desenhar();
+
+function adicionarTarefa(){
+    const texto = nomeTarefa.value.trim()
+    if(texto === "")return
+
+    const newTarefa = {
+        id:Date.now(),
+        titulo:texto,
+        concluida:false
+    }
+
+    tarefas.push(newTarefa)
+
+    nomeTarefa.value = ""
+    salvar()
+    desenhar()
+}
+
+function desenhar(){
+    lista.innerHTML = ""
+
+    tarefas.forEach((n,index) => {
+        const li = document.createElement("li")
+
+        li.innerHTML = `
+            ID: ${n.id} - Titulo: ${n.titulo} - concluida: ${n.concluida}
+            <button onclick="excluir(${index})">Apagar</button>
+        `
+
+        lista.appendChild(li)
+
+    });
+}
+
+function excluir(index){
+    tarefas.splice(index, 1)
+    salvar()
+    desenhar()
+}
