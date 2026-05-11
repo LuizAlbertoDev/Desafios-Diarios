3aa22e8c99307de65aa1a3fa47f8a7beef84219d
Luiz Alberto Huller da Silva <95250997+LuizAlbertoDev@users.noreply.github.com>
2026-05-10 21:30:59 -0300

    Create Desafio_048.js

---
+++ Desafios_JS/Desafio_048.js
@@ -0,0 +1,12 @@
+const produtos = [
+    { nome: "Teclado", preco: 150, categoria: "Eletrônicos" },
+    { nome: "Mouse", preco: 80, categoria: "Eletrônicos" },
+    { nome: "Cadeira", preco: 500, categoria: "Móveis" },
+    { nome: "Fone", preco: 200, categoria: "Eletrônicos" }
+];
+
+const eletronicos = produtos
+    .filter(p => p.categoria === "Eletrônicos")
+    .reduce((acc,atual) => acc + atual.preco,0)
+
+console.log(eletronicos)
