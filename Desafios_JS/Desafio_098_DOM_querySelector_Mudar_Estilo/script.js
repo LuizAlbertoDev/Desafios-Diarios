5d4ebb6f58b426d1868af8be314eee65efbf8308
Luiz Alberto Huller da Silva <95250997+LuizAlbertoDev@users.noreply.github.com>
2026-05-26 17:09:31 -0300

    Desafio_098_DOM_querySelector_Mudar_Estilo
    Projeto simples de manipulação de DOM usando querySelector em JavaScript. O desafio altera o texto, a cor e o tamanho da fonte do primeiro parágrafo da página dinamicamente.

---
+++ Desafios_JS/Desafio_098_DOM_querySelector_Mudar_Estilo/script.js
@@ -0,0 +1,7 @@
+const paragrafo = document.querySelector("p")
+
+paragrafo.textContent = "Selecionado com querySelector!"
+paragrafo.style.backgroundColor = "blue"
+paragrafo.style.fontSize = "24px"
+
+container.appendChild(paragrafo)
