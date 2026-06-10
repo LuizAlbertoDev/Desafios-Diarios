ba7d9b585c60b2c76dcf1c0cee77f325dc5332eb
Luiz Alberto Huller da Silva <95250997+LuizAlbertoDev@users.noreply.github.com>
2026-06-09 23:15:34 -0300

    feat: desafio 110 typeof

---
+++ Desafios_JS/Desafio_110_typeof.js
@@ -0,0 +1,9 @@
+let nome ="luiz"
+let idade = 32
+let estudante = true
+let vazio; 
+
+console.log(`Nome é do tipo: ${typeof nome}`)
+console.log(`Idade é do tipo: ${typeof idade}`)
+console.log(`Estudante é do tipo: ${typeof estudante}`)
+console.log(`Vazio é do tipo: ${typeof vazio}`)
