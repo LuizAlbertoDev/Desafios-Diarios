dbf07f213003dd38fc5839268fd500eb46c85431
Luiz Alberto Huller da Silva <95250997+LuizAlbertoDev@users.noreply.github.com>
2026-06-13 18:30:18 -0300

    feat: cria objeto aluno e manipula propriedades (desafio 124)

---
+++ Desafios_JS/Desafio_124_objetos_basico.js
@@ -0,0 +1,12 @@
+const aluno = {
+    nome:"luiz",
+    idade:31,
+    email:"luizalbertohullerbook@gmail.com",
+    cargo:"Desenvolvedor FullStack Jr"
+}
+
+console.log(`${aluno.nome}`)
+
+aluno.nota = 10
+
+console.log(aluno)
