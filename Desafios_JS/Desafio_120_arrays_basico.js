46cb1bc8a75f3878e18f9e3f17ae1874e36ebc3f
Luiz Alberto Huller da Silva <95250997+LuizAlbertoDev@users.noreply.github.com>
2026-06-13 16:48:32 -0300

    feat: manipula array com push e shift (desafio 120)

---
+++ Desafios_JS/Desafio_120_arrays_basico.js
@@ -0,0 +1,8 @@
+let listaDeCompras = ["Teclado","Mouse","Monitor"]
+
+listaDeCompras.push("Camera")
+listaDeCompras.shift()
+
+
+
+console.log(`${listaDeCompras}, com um total de ${listaDeCompras.length} Itens`)
