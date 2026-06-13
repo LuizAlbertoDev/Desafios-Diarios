282294352b0d5742e510c6e8723cfb30312eaa96
Luiz Alberto Huller da Silva <95250997+LuizAlbertoDev@users.noreply.github.com>
2026-06-13 18:33:53 -0300

    feat: extrai nomes de uma lista de objetos usando map (desafio 125)

---
+++ Desafios_JS/Desafio_125_array_de_objetos.js
@@ -0,0 +1,11 @@
+const turma = [
+    {nome: "Luiz",nota: 10},
+    {nome: "Maite",nota: 10},
+    {nome: "Stefani",nota: 10},
+    {nome: "Gregory",nota: 10}
+]
+
+const nomesDaTurma = turma.map(n => n.nome)
+
+console.log(turma)
+console.log(nomesDaTurma)
