76893367f6fbf003d03753c0110c9442790b23c4
Luiz Alberto Huller da Silva <95250997+LuizAlbertoDev@users.noreply.github.com>
2026-07-01 21:33:11 -0300

    feat: reformulando a resposta

--- Desafios_JS/Desafio_178_optional_chaining.js
+++ Desafios_JS/Desafio_178_optional_chaining.js
@@ -1,8 +1,8 @@
 const usuario1 = { nome: "Ana", endereco: { cidade: "Curitiba" } };
 const usuario2 = { nome: "Beto" };
 
-const cidadeUsuario1 = usuario1.endereco?.cidade
-const cidadeUsuario2 = usuario2.endereco?.cidade
+const cidadeUsuario1 = usuario1.endereco?.cidade ?? "Endereço não cadastrado"
+const cidadeUsuario2 = usuario2.endereco?.cidade ?? "Endereço não cadastrado"
 
 console.log(cidadeUsuario1)
 console.log(cidadeUsuario2)
